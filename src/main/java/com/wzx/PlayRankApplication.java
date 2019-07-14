1973f8f665d8de8f736d8553fc09aad8e5719357
Tsehsuan Wang <wangtsehsuan@gmail.com>
2019-07-14 13:21:35 +0900

    feat: docker-compose

--- src/main/java/com/wzx/PlayRankApplication.java
+++ src/main/java/com/wzx/PlayRankApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlayRankApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PlayRankApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PlayRankApplication.class, args);
+    }
 }
